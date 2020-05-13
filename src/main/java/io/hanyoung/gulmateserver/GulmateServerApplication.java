fe8672cd0d69436e4ff37c59ec804c8ffcb742f3
Hanyoung Jeong <8735132@gmail.com>
2020-05-13 15:45:52 +0900

    Gulmate server initialize

---
+++ src/main/java/io/hanyoung/gulmateserver/GulmateServerApplication.java
@@ -0,0 +1,13 @@
+package io.hanyoung.gulmateserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulmateServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GulmateServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/hanyoung/gulmateserver/GulmateServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hanyoung.gulmateserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulmateServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
